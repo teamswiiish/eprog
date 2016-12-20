a1d322bcf2ded9bc57e1042ae039ced47cba1be9
Johann Perry G. Tan <tan.johann@yahoo.com>
2016-12-20 08:28:24 +0800

    Create if_play.c

---
+++ if_play.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int num;
+printf("Enter number:");
+scanf("%d", &num);
+if(num%5 ==0){
+printf("ed");
+}
+if(num%3 ==0){
+printf("pogi");
+}
+if(num%5, 3 ==0){
+printf("Edpogi");
+}
+}
